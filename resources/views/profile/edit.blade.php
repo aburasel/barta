f3b7563ccd5df52450dad02ef6999bef470a4787
aburasel <aburasel@aci-bd.com>
2023-11-23 11:44:02 +0600

    renamed file

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,17 +4,17 @@
 @endsection
 
 @section('content')
-    @include('profile.partials.profile-form')
+    @include('profile.partials.profile_form')
     {{-- @include('profile.partials.update_profile_information_form') --}}
     <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
         <div class="max-w-xl">
-            @include('profile.partials.update-password-form')
+            @include('profile.partials.update_password_form')
         </div>
     </div>
 
     <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
         <div class="max-w-xl">
-            @include('profile.partials.delete-user-form')
+            @include('profile.partials.delete_user_form')
         </div>
     </div>
 @endsection
